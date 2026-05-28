f5f0b0e1276ce74519d3199e50846625ee87b89f
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-28 18:32:47 +0300

    style: add a semantic header for the heading

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,10 +4,12 @@ const Page = () => {
   return (
     <div className="min-h-screen">
       <div className="max-w-xl mx-auto w-full p-3 md:mt-5">
-        <h1 className="text-3xl md:text-5xl font-black">Bible Questions</h1>
-        <p className="text-lg text-stone-700 my-3 md:my-5">
-          Here are some cool Bible questions.
-        </p>
+        <header className="mb-7">
+          <h1 className="text-3xl md:text-5xl font-black">Bible Questions</h1>
+          <p className="text-lg text-stone-700 mt-3 md:mt-5">
+            Here are some cool Bible questions.
+          </p>
+        </header>
         <Accordion />
       </div>
     </div>
